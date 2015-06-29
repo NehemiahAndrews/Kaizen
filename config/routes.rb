f33e49313272ef86639e13d83686adaa4a12e645
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-06-29 01:20:14 +0000

    Added issues controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Kaizen::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  root 'issues#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
